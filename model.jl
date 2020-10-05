a9f71e81b94536286ce2411eae85e32a978cad40
Tran <lequytra@grinnell.edu>
2020-10-05 13:52:50 -0700

    Debug model

--- model.jl
+++ model.jl
@@ -1,4 +1,5 @@
 using Flux
+using LinearAlgebra
 
 mutable struct OutputModule 
 	output_layer::Chain
@@ -47,27 +48,41 @@ function (m::GRUBlock)(inp)
 	return inp 
 end
 
-struct Model 
-	graph_level
-	edge_level
+mutable struct GraphRNN 
+	graph_level::GRUBlock
+	edge_level::GRUBlock
+	GraphRNN(input_size, node_embedding_size, edge_embedding_size, node_hidden_size, node_output_size) = 
+		new(GRUBlock(input_size, node_embedding_size, node_hidden_size; output_size=node_output_size), 
+			GRUBlock(1, edge_embedding_size, node_output_size; output_size=1))
+
 end
 
-Flux.trainable(m::Model) = (m.graph_level, m.edge_level)
-Flux.reset!(m::Model) = Flux.reset!(m.graph_level) && Flux.reset!(m.edge_level)
+Flux.trainable(m::GraphRNN) = (m.graph_level, m.edge_level)
+Flux.reset!(m::GraphRNN) = Flux.reset!(m.graph_level) && Flux.reset!(m.edge_level)
 
-function (m::Model)(inp) 
-	println("Input size: $(size(inp))")
-	inp2 = m.graph_level(inp)
-	set_hidden!(edge_level, inp2)
-	edge_inp = fill(0.0, size(inp))
-	edge_inp[:, :, 1] .= 1.0
-	edge_inp[:, :, 2:end] = inp[:, :, 1:end - 1]
-	output = m.edge_level(edge_inp)
-	return output
+function (m::GraphRNN)(inp) 
+	n_nodes = size(inp[1], 2)
+	inp2 = m.graph_level.(inp)
+	inp2 = cat(inp2..., dims=2)
+
+	edge_inp = fill(1.0, (size(inp[1])..., size(inp)[1]))
+	partial = [item[:, 1:end-1] for item in inp]
+	edge_inp[:, 2:end, :] = cat([reshape(item, (size(item)..., 1)) for item in partial]..., dims=3)
+
+	edge_inp = cat([edge_inp[:, :, i] for i in 1:size(inp, 1)]..., dims=2)
+	edge_inp = [reshape(edge_inp[:, i], (1, size(edge_inp, 1))) for i in 1:size(edge_inp, 2)]
+	all_output = []
+	for i in 1:size(edge_inp, 1)
+		set_hidden!(m.edge_level, inp2[:, i])
+		push!(all_output, m.edge_level(edge_inp[i]))
+	end
+	all_output = cat(all_output..., dims=1)
+	all_output = reshape(all_output, size(inp, 1), n_nodes, :)
+	all_output = [transpose(all_output[i, :, :]) for i in 1:size(inp, 1)]
+	all_output = [reshape(item, (size(item)..., 1)) for item in all_output]
+	all_output = cat(all_output..., dims=3)
+	return all_output
 end
 
 
-function Model(input_size, node_embedding_size, edge_embedding_size, node_hidden_size, node_output_size) 
-	Model(GRUBlock(input_size, node_embedding_size, node_hidden_size; output_size=node_output_size),
-		GRUBlock(1, edge_embedding_size, node_output_size; output_size=1))
-end;
+# Hidden: hidden_size x timesteps
--- train.jl
+++ train.jl
@@ -18,11 +18,14 @@ function train(model, lr, dataloader, epochs, resume_from=1, checkpoints_folder=
 	loss(x, y) = Flux.Losses.logitcrossentropy(model(x), y)
 
 	for i in resume_from:epochs
+		println("start $i")
 		Flux.train!(loss, pms, dataloader, opt)
+		println("Finish 1 epoch.")
 
 		@save "$(checkpoints_folder)/model-$(now()).bson" model opt
-
+		println("Model saved.")
 		Flux.reset!(model)
+		println("Reset.")
 	end
 end
 
@@ -31,20 +34,19 @@ function main(config_path)
 	#TODO fill in main here including loading data, train, test etc. 
 	args = YAML.load_file(config_path)
 
-	x, y, len = load_dataset(args["data_path"]) |> gpu
-
-	println("Training on $(size(x, 1)) training examples.")
+	# x, y, len = load_dataset(args["data_path"]) |> gpu
+	x, y = [rand(28, 30) for i in 1:1000], rand(28, 30, 1000)
 
 	@show size(x), size(y)
 
 	dataloader = Flux.Data.DataLoader((x, y), batchsize=args["batch_size"], shuffle=true)
 	# TODO: default to 100 max_prev_node for now since data is encoded. 
 	max_prev_node = args["max_prev_node"] != -1 ? args["max_prev_node"] : 100
-	model = Model(max_prev_node, 
+	model = GraphRNN(max_prev_node, 
 		args["node_embedding_size"], 
 		args["edge_embedding_size"], 
 		args["node_hidden_size"], 
-		args["node_output_size"])  |> gpu
+		args["node_output_size"])  
 
 	if !isdir(args["checkpoints"])
 		mkdir(args["checkpoints"])
