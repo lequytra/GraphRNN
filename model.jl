596b4267a889f98bc17b31439b30c57fa1bd1e0e
Tran <lequytra@grinnell.edu>
2020-10-09 10:47:38 -0700

    Seem like model is working :)

--- model.jl
+++ model.jl
@@ -1,5 +1,5 @@
 using Flux
-using Zygote: @adjoint
+using Zygote: @adjoint, @showgrad, @nograd
 using LinearAlgebra
 
 mutable struct OutputModule 
@@ -36,7 +36,7 @@ function set_hidden!(m::GRUBlock, h)
 end
 
 Flux.trainable(gru::GRUBlock) = (gru.linear, gru.rnn, gru.output_module)
-Flux.reset!(gru::GRUBlock) = Flux.reset!(gru.rnn.chain)
+Flux.reset!(gru::GRUBlock) = Flux.reset!(gru.rnn.state)
 
 function (m::GRUBlock)(inp; hidden=nothing)
 	if m.has_input
@@ -62,35 +62,32 @@ mutable struct GraphRNN
 end
 
 Flux.trainable(m::GraphRNN) = (m.graph_level, m.edge_level)
-Flux.reset!(m::GraphRNN) = Flux.reset!(m.graph_level) && Flux.reset!(m.edge_level)
+function Flux.reset!(m::GraphRNN) 
+	Flux.reset!(m.graph_level)  
+	Flux.reset!(m.edge_level)
+end
 
-function (m::GraphRNN)(inp) 
-	n_nodes = size(inp[1], 2)
-	inp2 = m.graph_level.(inp)
-	inp2 = cat(inp2..., dims=2)
+function (m::GraphRNN)(inp)
+	n_nodes = size(inp, 2)
 
-	partial = [item[:, 1:end-1] for item in inp]
-	partial = cat([reshape(item, (size(item)..., 1)) for item in partial]..., dims=3)
-	edge_inp = cat([fill(1.0, (size(inp[1], 1), 1, size(inp)[1])), partial]..., dims=2)
+	inp2 = m.graph_level(inp)
 
-	edge_inp = cat([edge_inp[:, :, i] for i in 1:size(inp, 1)]..., dims=2)
-
-	edge_inp = [reshape(edge_inp[:, i], (1, size(edge_inp, 1))) for i in 1:size(edge_inp, 2)]
-
-	all_output = []
+	partial = inp[:, 1:end- 1]
+	edge_inp = cat([fill(1.0, size(inp, 1)), partial]..., dims=2)
+	edge_inp = reshape(edge_inp, (1, size(edge_inp)...))
+	edge_inp = [edge_inp[:, :, i] for i in 1:size(edge_inp, 3)]
 
 	inp2 = [inp2[:, i] for i in 1:size(inp2, 2)]
 	hidden_in = zip(inp2, edge_inp)
 	all_output = [m.edge_level(in_, hidden=hidden) for (hidden, in_) in hidden_in]
-	
+
 	all_output = cat(all_output..., dims=1)
-	all_output = reshape(all_output, size(inp, 1), n_nodes, :)
-	all_output = [transpose(all_output[i, :, :]) for i in 1:size(inp, 1)]
-	all_output = [reshape(item, size(item)..., 1) for item in all_output]
-	all_output = cat(all_output..., dims=3)
+	all_output = transpose(reshape(all_output, (n_nodes, :)))
+
 	println("done")
 	return all_output
 end
+
 
 @adjoint function Base.Iterators.Zip(is)
   Base.Iterators.Zip(is), Δ -> (collect(zip(Δ...))...,)
--- train.jl
+++ train.jl
@@ -1,5 +1,7 @@
 using Flux, CUDA
 import YAML
+using Dates
+using Statistics: mean
 using BSON: @save, @load
 
 include("model.jl")
@@ -13,16 +15,14 @@ function train(model, lr, dataloader, epochs, resume_from=1, checkpoints_folder=
 	else
 		opt = Flux.Optimise.ADAM(lr)
 	end
-
 	pms = Flux.params(model)
-	loss(x, y) = Flux.Losses.logitcrossentropy(model(x), y)
-
+	loss(x, y) = mean(Flux.Losses.logitcrossentropy.(model.(x), y))
+	
 	for i in resume_from:epochs
-		println("start $i")
 		Flux.train!(loss, pms, dataloader, opt)
 		println("Finish 1 epoch.")
 
-		@save "$(checkpoints_folder)/model-$(now()).bson" model opt
+		@save "$(checkpoints_folder)/model-$(Dates.now()).bson" model opt
 		println("Model saved.")
 		Flux.reset!(model)
 		println("Reset.")
@@ -34,8 +34,9 @@ function main(config_path)
 	#TODO fill in main here including loading data, train, test etc.
 	args = YAML.load_file(config_path)
 
-	# x, y, len = load_dataset(args["data_path"]) |> gpu
-	x, y = [rand(28, 30) for i in 1:1000], rand(28, 30, 1000)
+	x, y, len = load_dataset(args["data_path"]) |> gpu
+	x, y = [x[:, :, i] for i in 1:size(x, 3)], [y[:, :, i] for i in 1:size(y, 3)]
+	# x, y = [rand(28, 30) for i in 1:2], [rand(28, 30) for i in 1:2]
 
 	@show size(x), size(y)
 
@@ -43,11 +44,11 @@ function main(config_path)
 	# TODO: default to 100 max_prev_node for now since data is encoded.
 	max_prev_node = args["max_prev_node"] != -1 ? args["max_prev_node"] : 100
 
-	model = Model(max_prev_node,
+	model = GraphRNN(max_prev_node,
 		args["node_embedding_size"],
 		args["edge_embedding_size"],
 		args["node_hidden_size"],
-		args["node_output_size"])  |> gpu
+		args["node_output_size"])  
 
 	if !isdir(args["checkpoints"])
 		mkdir(args["checkpoints"])
@@ -59,15 +60,15 @@ end
 
 # main("configs/test.yaml")
 
-function test_rnn_epoch (epoch, rnn, output, max_num_node, max_prev_node, test_batch_size=16)
-	# TODO: init hidden layers and eval output
+# function test_rnn_epoch (epoch, rnn, output, max_num_node, max_prev_node, test_batch_size=16)
+# 	# TODO: init hidden layers and eval output
 
-	# generate graphs
-	y_prediction_long = fill(0.0, test_batch_size, max_num_node, max_prev_node) #|> gpu
-	x_steps = fill(1.0, test_batch_size, 1, max_prev_node)
+# 	# generate graphs
+# 	y_prediction_long = fill(0.0, test_batch_size, max_num_node, max_prev_node) #|> gpu
+# 	x_steps = fill(1.0, test_batch_size, 1, max_prev_node)
 
-	for i in 1:max_num_node
-		h = rnn(x_steps)
+# 	for i in 1:max_num_node
+# 		h = rnn(x_steps)
 
-	end
-end
+# 	end
+# end
